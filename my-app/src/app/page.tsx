62db3bf6daf01834044bdabe07723f0c8026c91c
Oliver Forral <intrepion@gmail.com>
2024-04-13 18:52:37 -0600

    NEXT.JS: READING FROM THE DATABASE

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <h2>Home</h2>;
+import { prisma } from "@/lib/prisma";
+
+const Home = async () => {
+  const posts = await prisma.post.findMany();
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <h2>Home</h2>
+
+      <ul className="flex flex-col gap-y-2">
+        {posts.map((post) => (
+          <li key={post.id}>{post.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
