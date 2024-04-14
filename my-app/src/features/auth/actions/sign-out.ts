3067d5ba93bba4876beaa351aed8297e861cb185
Oliver Forral <intrepion@gmail.com>
2024-04-13 19:58:00 -0600

    SIGN OUT WITH SERVER ACTION

--- my-app/src/features/auth/actions/sign-out.ts
+++ my-app/src/features/auth/actions/sign-out.ts
@@ -1,8 +1,26 @@
 "use server";
 
-const signOut = async () => {
-  // TODO - Implement sign out logic
-  console.log("Sign Out");
-};
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { lucia } from "@/lib/lucia";
+import { getAuth } from "../queries/get-auth";
 
-export { signOut };
+export const signOut = async (_formData: FormData) => {
+  const { session } = await getAuth();
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  redirect("/sign-in");
+};
